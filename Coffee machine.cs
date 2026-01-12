2b550ed3b850bd9fe984ebc7cb71c47257f6e72a
Poonnapob <poonnapop.s@kkumail.com>
2026-01-12 21:32:18 +0700

    Add files via upload

---
+++ Coffee machine.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_VendingMachine
+{
+    public class VendingMachine
+    {
+        int coffee = 0;
+        int water = 0;
+        int milk = 0;
+        int chokkolat = 0;
+        //constructor
+        public VendingMachine(int coffee, int water,int milk,int  chokkolat)
+        {
+            this.coffee = coffee;
+            this.water = water;
+            this.milk = milk;
+            this.chokkolat = chokkolat;
+
+        }
+
+        public void Buyblackcoffee()
+        {
+            int co = 20;
+            int wa = 300;
+            if (CheckStock(co, wa, 0, 0))
+            {
+                UseStock(co, wa, 0, 0);
+                Console.WriteLine("Enjoy your black coffee!");
+            }
+            else
+            {
+                Console.WriteLine("Sorry, not enough stock for black coffee.");
+            }
+        }   
+        public void Buymokca() 
+        {
+            int co = 20;
+            int wa = 300;
+            int ckl = 10;
+            if (CheckStock(co, wa, 0, ckl))
+            {
+                UseStock(co, wa, 0, ckl );
+                Console.WriteLine("Enjoy your mokca!");
+            }
+            else
+            {
+                Console.WriteLine("Sorry, not enough stock for mokca.");
+            }
+        }
+        public void Buylatte() 
+        {
+            int co = 20;
+            int wa = 300;
+            int mk = 10;
+            if (CheckStock(co, wa, mk, 0))
+            {
+                UseStock(co, wa, mk , 0);
+                Console.WriteLine("Enjoy your latte!");
+            }
+            else
+            {
+                Console.WriteLine("Sorry, not enough stock for latte.");
+            }
+        }
+        public void Buychokkolat(int amount)
+        {
+            int ckl = 20;
+            int wa = 300;
+            if (CheckStock(0, wa, 0, ckl))
+            {
+                UseStock(0, wa, 0, ckl);
+                Console.WriteLine("Enjoy your chokkolat!");
+            }
+            else
+            {
+                Console.WriteLine("Sorry, not enough stock for chokkolat.");
+            }
+        }
+        public void ShowStock()
+        {
+            Console.WriteLine("Current stock:");
+            Console.WriteLine("coffee: " + this.coffee);
+            Console.WriteLine("Water: " + this.water);
+            Console.WriteLine("Milk: " + this.milk);
+            Console.WriteLine("Chokkolat: " + this.chokkolat);
+        }
+        public void IncreaseStock(int co, int wa, int mk, int ckl, int no)
+        {
+            // Fix: Read input from Console, parse to int
+            Console.Write("Enter stock item number to increase (7: coffee, 8: water, 9: milk, 10: chokkolat): ");
+            string input = Console.ReadLine();
+            int itemNo;
+            if (int.TryParse(input, out itemNo))
+            {
+                if (itemNo == 7)
+                {
+                    this.coffee += 1000;
+                }
+                else if (itemNo == 8)
+                {
+                    this.water += 10000;
+                }
+                else if (itemNo == 9)
+                {
+                    this.milk += 1000;
+                }
+                else if (itemNo == 10)
+                {
+                    this.chokkolat += 1000;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid input.");
+            }
+            ShowStock();
+        }
+        public void UseStock(int co, int wa, int mk, int ckl)
+        {
+            this.coffee -= co;
+            this.water -= wa;
+            this.milk -= mk;
+            this.chokkolat -= ckl;
+            ShowStock();
+        }
+        private bool CheckStock(int co, int wa, int mk, int ckl)
+        {
+            return (this.coffee >= co && this.water >= wa && this.milk >= mk && this.chokkolat >= ckl);
+        }
+        
+            
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_VendingMachine
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //create object
+            VendingMachine vendingMachineA = new VendingMachine(0, 10000, 0, 0);
+            //menu interface
+            while (true)
+            {
+                Console.WriteLine("--------------------------------");
+                Console.WriteLine("Wellcome to Coffee Machine");
+                Console.WriteLine("--------------------------------");
+                Console.WriteLine("What do you buy?");
+                Console.WriteLine("----------------------");
+                Console.WriteLine("1:Black Coffee");
+                Console.WriteLine("----------------------");
+                Console.WriteLine("2:Mokca");
+                Console.WriteLine("----------------------");
+                Console.WriteLine("3:Latte");
+                Console.WriteLine("----------------------");
+                Console.WriteLine("4:Chokkocat");
+                Console.WriteLine("--------------------------------");
+                Console.WriteLine("5:Show stock");
+                Console.WriteLine("6:Increase stock");
+                Console.WriteLine("----------------------");
+                Console.WriteLine("0:Exit program");
+                Console.WriteLine("----------------------");
+                Console.WriteLine("--------------------------------");
+
+                string input = Console.ReadLine();
+                switch (input)
+                {
+                    
+                    case "1":
+                        vendingMachineA.Buyblackcoffee();
+                        break;
+                    case "2":
+                        vendingMachineA.Buymokca();
+                        break;
+                    case "3":
+                        vendingMachineA.Buylatte();
+                        break;
+                    case "4":
+                        vendingMachineA.Buychokkolat(1);
+                        break;
+                    case "5":
+                        vendingMachineA.ShowStock();
+                        break;
+                    case "6":
+                            vendingMachineA.IncreaseStock(0, 0, 0, 0, 0);
+                        break;
+                    case "0":
+                        Environment.Exit(0);
+                        break;
+
+                    default:
+                        Console.WriteLine("Wrong input try again");
+                        break;
+
+                }
+            }
+        }
+    }
+}
